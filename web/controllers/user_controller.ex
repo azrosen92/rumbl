bd0c9588c8805314aaddebbe28c9cc11b163fbc7
azrosen92 <azrosen92@gmail.com>
2017-04-08 10:05:10 -0400

    second commit

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,6 +3,8 @@ defmodule Rumbl.UserController do
 
   alias Rumbl.User
 
+  plug :authenticate_user when action in [:index, :show]
+
   def index(conn, _params) do
     users = Repo.all(User)
     render conn, "index.html", users: users
@@ -19,10 +21,11 @@ defmodule Rumbl.UserController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
     case Repo.insert(changeset) do
       {:ok, user} ->
         conn
+        |> Rumbl.Auth.login(user)
         |> put_flash(:info, "#{user.name} created!")
         |> redirect(to: user_path(conn, :index))
       {:error, changeset} ->
