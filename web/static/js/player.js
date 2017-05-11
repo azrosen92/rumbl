5ad4bdb6ea1d23da098ee51ee39caa190adbcf47
Aaron Rosen <azrosen92@gmail.com>
2017-05-10 22:45:57 -0400

    page 174

--- web/static/js/player.js
+++ web/static/js/player.js
@@ -1,28 +1,29 @@
-let Player = {
+let Player = { 
   player: null,
-
-  init(domId, playerId, onReady) {
-    window.onYoutubeIframeAPIReady = () => {
-      this.onIframeReady(domId, playerId, onReady)
+  
+  init(domId, playerId, onReady){
+    window.onYouTubeIframeAPIReady = () => {
+      this.onIframeReady(domId, playerId, onReady) 
     }
-    let youtubeScriptTag = document.createElement("script")
-    youtubeScriptTag.src = "//wwww.youtube.com/iframe_api"
+    
+    let youtubeScriptTag = document.createElement("script") 
+    youtubeScriptTag.src = "//www.youtube.com/iframe_api" 
     document.head.appendChild(youtubeScriptTag)
   },
 
-  onIframeReady(domId, playerId, onReady) {
+  onIframeReady(domId, playerId, onReady) { 
     this.player = new YT.Player(domId, {
-      height: "360",
-      width: "420",
-      videoId: playerId,
+      height: "360", 
+      width: "420", 
+      videoId: playerId, 
       events: {
         "onReady": (event => onReady(event) ),
-        "onStateChange": (event => this.onPlayerStateChange(event) )
+        "onStateChange": (event => this.onPlayerStateChange(event) ) 
       }
-    })
+    }) 
   },
 
-  onPlayerStateChange(event) { },
+  onPlayerStateChange(event){ },
 
   getCurrentTime() { return Math.floor(this.player.getCurrentTime() * 1000) },
   seekTo(millisec) { return this.player.seekTo(millisec / 1000) }
